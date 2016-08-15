3b71132744a79fe08b017bb6b67618b771db6c13
acchanAlexander <AtariYoshihiro@next-group.jp>
2016-08-15 14:09:16 +0900

    コンストラクタについて学んだ

---
+++ atariyo/Todo.java
@@ -0,0 +1,20 @@
+public class Todo{
+
+    String str;
+    int num;
+
+    // constractor
+    Todo() {
+        this.str = 'mozi';
+        this.num = 123;
+    }
+
+    public static void main(String[] args){
+        System.out.println("Hello, world.");
+    }
+
+    // todo一覧を表示する
+    public static void showList () {
+    
+    }
+}
