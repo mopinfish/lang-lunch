c69a7b230870f5ef8a17105d0089ec5a1375217d
acchanAlexander <AtariYoshihiro@next-group.jp>
2016-09-05 14:23:04 +0900

    ファイルへの書き込み

--- atariyo/Todo.java
+++ atariyo/Todo.java
@@ -1,3 +1,9 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
+
 public class Todo{
 
     String str;
@@ -5,12 +11,28 @@ public class Todo{
 
     // constractor
     Todo() {
-        this.str = 'mozi';
+        this.str = "mozi";
         this.num = 123;
     }
 
     public static void main(String[] args){
         System.out.println("Hello, world.");
+
+    }
+
+    // todo一覧に追加する
+    public static void addList () {
+        // とりあえずファイルに書き込むサンプル
+        // ここでtodoのjsonを書き込む
+        try{
+          File file = new File("todolist.txt");
+          FileWriter filewriter = new FileWriter(file);
+
+          filewriter.write("atariyo");
+          filewriter.close();
+        }catch(IOException e){
+          System.out.println(e);
+        }
     }
 
     // todo一覧を表示する
