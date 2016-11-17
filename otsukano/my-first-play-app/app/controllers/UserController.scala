ae8ccbcf553a7d656be41a389513cd405913b2d7
otsukano <otsukanoboru@next-group.jp>
2016-11-17 21:47:44 +0900

    [my-first-scala-app] add create and udpate action.

--- otsukano/my-first-play-app/app/controllers/UserController.scala
+++ otsukano/my-first-play-app/app/controllers/UserController.scala
@@ -71,12 +71,50 @@ class UserController @Inject()(val dbConfigProvider: DatabaseConfigProvider,
   /**
    * 登録実行
    */
-  def create = TODO
+  def create = Action.async { implicit rs =>
+    // リクエストの内容をバインド
+    userForm.bindFromRequest.fold(
+      // エラーの場合
+      error => {
+        db.run(Companies.sortBy(t => t.id).result).map { companies =>
+          BadRequest(views.html.user.edit(error, companies))
+        }
+      },
+      // OKの場合
+      form => {
+        // ユーザを登録
+        val user = UserRow(0, form.name, form.companyId)
+        db.run(Users += user).map { _ =>
+          // 一覧画面へリダイレクト
+          Redirect(routes.UserController.list)
+        }
+      }
+    )
+  }
 
   /**
    * 更新実行
    */
-  def update = TODO
+  def update = Action.async { implicit rs =>
+    // リクエストの内容をバインド
+    userForm.bindFromRequest.fold(
+      // エラーの場合は登録画面に戻す
+      error => {
+        db.run(Companies.sortBy((t => t.id).result).map { companies =>
+          BadRequest(views.html.user.edit(error, companies))
+        }
+      },
+      // OKの場合は登録を行い一覧画面にリダイレクトする
+      form => {
+        // ユーザ情報を更新
+        val user = UserRow(form.id.get, form.name, form.companyId)
+        db.run(Users.filter(t => t.id === user.id.bind).update(user)).map { _ =>
+          // 一覧画面へリダイレクト
+          Redirect(routes.UserController.list)
+        }
+      }
+    )
+  }
 
   /**
    * 削除実行
