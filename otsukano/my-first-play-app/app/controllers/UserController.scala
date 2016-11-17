6fa17a65fc2d5cdf1d99692068a359d5e7ec780b
otsukano <otsukanoboru@next-group.jp>
2016-11-17 21:23:39 +0900

    [my-first-scala-app] add user list page and edit page.

--- otsukano/my-first-play-app/app/controllers/UserController.scala
+++ otsukano/my-first-play-app/app/controllers/UserController.scala
@@ -12,6 +12,20 @@ import javax.inject.Inject
 import scala.concurrent.Future
 import slick.driver.H2Driver.api._
 
+object UserController {
+  // フォームの値を格納するケースクラス
+  case class UserForm(id: Option[Long], name: String, companyId: Option[Int])
+
+  // formから送信されたデータ <=> ケースクラスの変換を行う
+  val userForm = Form(
+    mapping(
+      "id"        -> optional(longNumber),
+      "name"      -> nonEmptyText(maxLength = 20),
+      "companyId" -> optional(number)
+    )(UserForm.apply)(UserForm.unapply)
+  )
+}
+
 class UserController @Inject()(val dbConfigProvider: DatabaseConfigProvider,
                                val messagesApi: MessagesApi) extends Controller
     with HasDatabaseConfigProvider[JdbcProfile] with I18nSupport {
@@ -19,12 +33,40 @@ class UserController @Inject()(val dbConfigProvider: DatabaseConfigProvider,
   /**
    * 一覧表示
    */
-  def list = TODO
+  def list = Action.async { implicit rs =>
+    // IDの照準にすべてのユーザ情報を取得
+    db.run(Users.sortBy(t => t.id).result).map { users =>
+      // 一覧画面を表示
+      Ok(views.html.user.list(users))
+    }
+  }
+
+  // コンパニオンオブジェクトに定義したFormを参照するためにimport文を追加
+  import UserController._
 
   /**
    * 編集画面表示
    */
-  def edit(id: Option[Long]) = TODO
+  def edit(id: Option[Long]) = Action.async { implicit rs =>
+    val form = if (id.isDefined) {
+      // リクエストパラメータにIDが存在する場合
+      // IDからユーザ情報を1件取得
+      db.run(Users.filter(t => t.id === id.get.bind).result.head).map { user =>
+        // 値をフォームに詰める
+        userForm.fill(UserForm(Some(user.id), user.name, user.companyId))
+      }
+    } else {
+      // リクエストパラメータにIDが存在しない場合
+      Future { userForm }
+    }
+
+    form.flatMap { form =>
+      // 会社一覧を取得
+      db.run(Companies.sortBy(_.id).result).map { companies =>
+        Ok(views.html.user.edit(form, companies))
+      }
+    }
+  }
 
   /**
    * 登録実行
