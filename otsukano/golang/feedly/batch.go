d1827d632e86ea775e97ba141b3e224c4d73bfc8
mopinfish <mopinfish@users.noreply.github.com>
2017-06-05 13:47:14 +0900

    Feedly batch (#3)
    [lang-lunch]feedlyの特定タグのついた記事をランダムにCWに通知するバッチ

---
+++ otsukano/golang/feedly/batch.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+  "fmt" 
+  "reflect"
+)
+
+func main() {
+  token := "A3TOHrc4fbTpfhclHV7_4NUx76y7acBd71A-MrwjZJEQulgKNG6Vd1cZMCDKUeh9Xi827P_iLyfAm6w2bg37EE4GVj8KzgpJgbKSux5CXwCrdKD_RArRglq7dgbLodUK6-gIoTSKF4XCq1dl28RUsJeG3Nh5egz0SeKovmVE98IRgNK7QTsVzRiCqd34lgsUG_HIvOeb5_Hgz99dD6vIM5yBlUazAug:feedlydev"
+  fmt.Println(token)
+
+  username := `otsukano`
+  fmt.Printf(username + "\n")
+
+  fmt.Println(reflect.TypeOf(username))
+  fmt.Printf("こんにちはせかい\n")
+
+  const v = iota // constキーワードが出現する度に、iotaは置き直されます。ここではv == 0です。
+
+  const ( 
+      e, f, g = iota, iota, iota //e=0,f=0,g=0 iotaの同一行は同じです
+  )
+  fmt.Println(e, f, g)
+
+  arr := [3]int{1, 2, 3}
+  fmt.Println(arr)
+  fmt.Println(reflect.TypeOf(arr)) 
+
+  arr2 := []int{1, 2, 3}
+  fmt.Println(arr2)
+  fmt.Println(reflect.TypeOf(arr2)) 
+
+  // keyを文字列で宣言します。値はintとなるディクショナリです。この方法は使用される前にmakeで初期化される必要があります。
+  // var numbers map[string]int
+  // もうひとつのmapの宣言方法
+  numbers := make(map[string]int)
+  numbers["one"] = 1  //代入
+  numbers["ten"] = 10 //代入
+  numbers["three"] = 3
+
+  fmt.Println("３つ目の数字は: ", numbers["three"]) // データの取得
+  // "３つ目の数字は： 3"という風に出力されます。
+  // ディクショナリを初期化します。
+  rating := map[string]float32{"C":5, "Go":4.5, "Python":4.5, "C++":2 }
+  // mapは２つの戻り値があります。２つ目の戻り値では、もしkeyが存在しなければ、okはfalseに、存在すればokはtrueになります。
+  csharpRating, ok := rating["C#"]
+  if ok {
+    fmt.Println("C# is in the map and its rating is ", csharpRating)
+  } else {
+    fmt.Println("We have no rating associated with C# in the map")
+  }
+
+  delete(rating, "C")  // keyがCの要素を削除します。
+  fmt.Println(rating)
+
+  i := 3
+  switch i {
+  case 1:
+    fmt.Println("i is equal to 1")
+  case 2, 3, 4:
+    fmt.Println("i is equal to 2, 3 or 4")
+  case 10:
+    fmt.Println("i is equal to 10")
+  default:
+    fmt.Println("All I know is that i is an integer")
+  }
+}
---
+++ otsukano/golang/feedly/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+  "os"
+  "fmt"
+  "net/http"
+  "github.com/m0a/easyjson"
+  "math/rand"
+  chatwork "github.com/griffin-stewie/go-chatwork"
+)
+
+var api = "https://teratail.com/api/v2"
+var token = os.Getenv("FEEDLY_TOKEN")
+var user_id = os.Getenv("FEEDLY_USER_ID")
+
+func get() ([]map[string]string, error) {
+  var err error
+  url := "https://cloud.feedly.com/v3/streams/contents?streamId=user/" + user_id + "/tag/Read%20For%20Lator&count=90"
+  req, _ := http.NewRequest("GET", url, nil)
+  req.Header.Set("Authorization", token)
+
+  // setup feedly api client
+  client := new(http.Client)
+  response, res_err := client.Do(req)
+  if res_err != nil {
+    err = fmt.Errorf("Invalid responses")
+  }
+
+  defer response.Body.Close()
+  jsonData, err := easyjson.NewEasyJson(response.Body)
+
+  var data []map[string]string
+
+  for k,_ := range jsonData.K("items").RangeObjects() {
+    title, title_err := jsonData.K("items").K(k).K("title").AsString()
+
+    if title_err != nil {
+      err = fmt.Errorf("Invalid responses")
+    }
+
+    url, url_err := jsonData.K("items").K(k).K("alternate").RangeObjects()[0].K("href").AsString()
+    if url_err != nil {
+      err = fmt.Errorf("Invalid responses")
+    }
+
+    item := map[string]string{"title": title, "url": url}
+    data = append(data, item)
+  }
+
+  return data, err
+}
+
+func shuffle (list []map[string]string) []map[string]string {
+  for i := len(list); i > 1; i-- {
+    j := rand.Intn(i)          // 0～(i-1) の乱数発生
+    list[i - 1], list[j] = list[j], list[i - 1]
+  }
+  return list
+}
+
+func makeBody (data []map[string]string) string {
+  var body string
+  for k, item := range data {
+    if k > 3 {
+      break
+    }
+    body += item["title"] + "\n" + item["url"] + "\n\n"
+  }
+  return body
+}
+
+func send(message string) error {
+  token := os.Getenv("CHATWORK_TOKEN")
+  chatwork := chatwork.NewClient(token)
+  endpoint := "/rooms/74123818/messages"
+  params :=  map[string]string{"body": message}
+fmt.Println(params)
+  chatwork.Post(endpoint, params)
+  return nil
+}
+
+func main() {
+  data, err := get()
+  if err != nil {
+    fmt.Fprintf(os.Stderr, "%s\n", err)
+    os.Exit(1)
+  }
+  shuf_data := shuffle(data)
+  body := makeBody(shuf_data)
+  if err := send(body); err != nil {
+    fmt.Fprintf(os.Stderr, "%s\n", err)
+    os.Exit(1)
+  }
+}
