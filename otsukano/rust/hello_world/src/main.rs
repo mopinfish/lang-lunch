28976cd8018cb09d98e0c084804d6a08dfe44df5
otsukano <OtsukaNoboru@LIFULL.com>
2018-01-22 13:51:54 +0900

    [lang] rust add fibonatti func

--- otsukano/rust/hello_world/src/main.rs
+++ otsukano/rust/hello_world/src/main.rs
@@ -1,5 +1,37 @@
+use std::io;
+use std::io::prelude::*;
+
+trait Fruit {
+    fn get_size(&self) -> u64;
+}
 struct Apple {
     size: u64
+}
+struct Pine {
+    size: u64
+}
+
+impl Fruit for Apple {
+    fn get_size(&self) -> u64 {
+        println!("apple");
+        self.size
+    }
+}
+
+impl Fruit for Pine {
+    fn get_size(&self) -> u64 {
+        println!("pine");
+        self.size
+    }
+}
+
+fn fib(i: i32) -> i32 {
+    let mut sum: i32 = 0;
+    if i <= 1 {
+        i
+    } else {
+        fib(i - 1) + fib(i - 2)
+    }
 }
 fn some_calc(i: i32) -> i32 {
     if i == 0 {
@@ -17,8 +49,9 @@ fn factorial() -> i32 {
     sum
 }
 fn main() {
-  let apple = Apple { size: 1 };
-  println!("{}", factorial());
-  println!("{}", apple.size);
-
+    println!("{}", fib(10));
+//  let apple = Apple { size: 3 };
+//  println!("{}", factorial());
+//  println!("{}", apple.size);
+//  println!("{}", apple.get_size());
 }
